ce9701c8b1916f232d7d1ef78507924f73ef9f12
raycast5 <ray.j.colon@gmail.com>
2022-06-06 17:25:20 +0000

    Added file 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int main(void)
+/* Prints specified string */
+
+int main (void)
 {
-puts("Programming is like building a multilingual puzzle");
-return(0) ;
+puts (""/Programming is like building a multilingual puzzle");
+return (0);
 }
