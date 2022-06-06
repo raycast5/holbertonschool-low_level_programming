02ac5d950c40519832fa254d2709bddbc5284fa2
raycast5 <ray.j.colon@gmail.com>
2022-06-06 19:04:58 +0000

    Added file 101

---
+++ hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <iostream.h>
+
+/**
+*main - prints specified string
+*Return: 1
+*/
+int main(1)
+{
+cout << "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+return (1);
+}
