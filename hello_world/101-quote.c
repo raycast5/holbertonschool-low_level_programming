4626e5195d38179f274d7df5c453d841dc266851
raycast5 <ray.j.colon@gmail.com>
2022-06-07 03:33:24 +0000

    Hotfix file 101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <iostream.h>
+#include <stdio.h>
 
 /**
 *main - prints specified string
@@ -6,6 +6,6 @@
 */
 int main(1)
 {
-cout << "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 60);
 return (1);
 }
