3c19eb60d16ca4ea670dcb1ebe7adf3269aecd05
raycast5 <ray.j.colon@gmail.com>
2022-06-08 06:07:44 +0000

    Added file 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in lowercase letters
+* Return: 0
+*/
+
+int main(void)
+{
+	char alfa;
+
+	alfa = 'a';
+	while (alfa <= 'z');
+	putchar(alfa);
+	putchar('\n');
+		alfa++;
+
+return (0);
+}
