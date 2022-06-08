229abff42924b3e108358e4305681d6a3b7d5cf9
raycast5 <ray.j.colon@gmail.com>
2022-06-08 04:06:38 +0000

    Fixed file 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
 * main - prints if the number given is negative or positve
-* Returns: 0
+* Return: 0
 */
 
 int main(void)
@@ -15,12 +15,12 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	printf("%i ", n);
-		if(n > 0)
+		if (n > 0)
 			printf("is positive\n");
-		else if(n == 0)
+		else if (n == 0)
 			printf("is zero\n");
 		else
-			printf("is negative\n");		
+			printf("is negative\n");
 
 	return (0);
 }
