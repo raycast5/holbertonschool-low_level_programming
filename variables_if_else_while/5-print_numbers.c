e0968bb4471111588b804ea8c51b9a1d0aab321f
raycast5 <ray.j.colon@gmail.com>
2022-06-08 08:34:59 +0000

    Added file 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - prints numbers 1-9
+* Return: 0
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		printf("%i", n);
+		n++;
+	}
+	printf("\n");
+return (0);
+}
