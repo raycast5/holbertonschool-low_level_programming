8cb1b750dd5f2222356366ea6b933a0efa59e352
raycast5 <ray.j.colon@gmail.com>
2022-06-09 17:49:51 +0000

    Added file 3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,11 +2,12 @@
 
 /**
 * _islower - determines if c is lowercase
+* c - is char
 * Return: 1 if c is lowercase, 0 if otherwise.
 */
 
 int _islower(int c)
-{	
+{
 	if ((c >= 'a') && (c <= 'z'))
 	return (1);
 	else
