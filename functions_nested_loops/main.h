9f9e186dcb9f6cc085fc591e409f94b566f69463
raycast5 <ray.j.colon@gmail.com>
2022-06-09 06:03:57 +0000

    Hotfix

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -7,7 +7,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-extern int _putchar(char c);
+extern int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
