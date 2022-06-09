8a67c22182881031cbedadd1e04d41dabdbc6558
raycast5 <ray.j.colon@gmail.com>
2022-06-09 15:11:05 +0000

    Added file 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints alphabet 10 times
+* Return: Always 0 (Success)
+*/
+
+void print_alphabet_x10(void);
+{
+	int i;
+	
+	for (i = 1; i < 10; ++i)
+		print_alphabet()
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,3 +3,5 @@
 int _putchar(char c);
 
 void print_alphabet(void);
+
+void print_alphabe_x10(void);
