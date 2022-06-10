619e70ea90312677d8e8848f7db6f2ec40838365
raycast5 <ray.j.colon@gmail.com>
2022-06-10 19:32:12 +0000

    Added file 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,10 +8,10 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		for (; n  >= 98; n--)
+		for (; n  >= 97; n--)
 		{
 			printf("%d, ", n);
-			if (n == 98)
+			if (n == 97)
 			{
 				printf("98\n");
 			}
@@ -19,18 +19,18 @@ void print_to_98(int n)
 	}
 	else if (n < 98)
 	{
-		for (; n <= 98; n++)
+		for (; n <= 97; n++)
 		{
 			printf("%d, ", n);
-			if (n == 98)
+			if (n == 97)
 			{	
 				printf("98\n");
 			}
 		}
 	}
-	else if (n == 98)
+	else
 	{
-		printf("98\n");
+		printf("%d\n",n);
 	}
 
 }
