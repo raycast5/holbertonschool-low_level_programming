43016b5764a876c00614b4903606f8bbbe73c978
raycast5 <ray.j.colon@gmail.com>
2022-06-10 19:36:26 +0000

    Added file 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,10 +8,10 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		for (; n  >= 97; n--)
+		for (; n  >= 99; n--)
 		{
 			printf("%d, ", n);
-			if (n == 97)
+			if (n == 99)
 			{
 				printf("98\n");
 			}
