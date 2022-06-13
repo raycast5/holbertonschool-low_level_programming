e537bbcacb71d7904e75ae90658eaf2198d853cb
raycast5 <ray.j.colon@gmail.com>
2022-06-13 16:49:29 +0000

    Added file 2

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -4,11 +4,10 @@
  * mul - Multiplies @a by @b
  * @a: one of the values to be multiplied
  * @b: one of the values to be multiplied
- * @res:  result
- * Return:  @res
+ * Return:  res
  */
 int mul(int a, int b)
 {
 	int res = (a * b);
-    return (res);
+	return (res);
 }
