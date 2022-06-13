24ac6dd68e5773c1599abc4135eb4b67585eec6c
raycast5 <ray.j.colon@gmail.com>
2022-06-13 16:47:51 +0000

    Added file 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies @a by @b
+ * @a: one of the values to be multiplied
+ * @b: one of the values to be multiplied
+ * @res:  result
+ * Return:  @res
+ */
+int mul(int a, int b)
+{
+	int res = (a * b);
+    return (res);
+}
