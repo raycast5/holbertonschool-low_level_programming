b3bed1ebfc914a02cbbda302dd41ec21774787c2
raycast5 <ray.j.colon@gmail.com>
2022-06-13 23:19:39 +0000

    Added file 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1-100, prints "Fizz" for multiples of 3, "Buzz" for multiples of 5, "FizzBuzz" for both
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 15) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+		printf("%i ", i);
+		}
+	}
+printf("\n");
+
+return (0);
+}
