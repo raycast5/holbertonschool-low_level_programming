d7ab5d9ed0ccced08c18fafcfd06a1733be27615
raycast5 <ray.j.colon@gmail.com>
2022-06-13 21:33:26 +0000

    Added file 6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -2,16 +2,16 @@
 
 /**
  * print _line - Prints a line @n number of times
- * @c: number of lines to be printed
+ * @n: number of lines to be printed
  * Return: Always 0.
  */
 void print_line(int n)
 {
 	int i;
-	
+
 	if (n > 0)
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
