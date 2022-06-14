a88087c0f9a03dbbc3430639faf0d853fd9d8e89
raycast5 <ray.j.colon@gmail.com>
2022-06-14 18:32:25 +0000

    Added file 2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,13 @@
 
 /**
  * _strlen - determines the length of a string
- *
+ * @s: string to be counted
  * Return: string length
  */
 int _strlen(char *s)
 {
 	int i;
-	
+
 	for (i = 0; (*(s + i) != '\0'); ++i);
 	return (i);
 }
