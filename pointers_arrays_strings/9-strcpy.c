02efcdd56c77f084496860fee6f1c013fb1bd793
raycast5 <ray.j.colon@gmail.com>
2022-06-15 04:53:24 +0000

    Added file 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strcpy - copies string entirely
+ *@dest: destination
+ *@src: source
+ *
+ *Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (i = 0; src[i]; i++)
+	{
+
+	}
+	dest[i] = src[i];
+	dest[i] = src[i];
+
+	return (dest);
+}
