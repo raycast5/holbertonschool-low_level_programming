a6ba87678af2d6528f96a38e96ef357a516bd7df
raycast5 <ray.j.colon@gmail.com>
2022-06-15 05:08:46 +0000

    Added file 9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -12,9 +12,8 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	for (i = 0; src[i] <= '\0'; i++)
+	for (i = 0; src[i]; i++)
 	{
-		dest[i] = src[i];
 		dest[i] = src[i];
 	}
 	return (dest);
