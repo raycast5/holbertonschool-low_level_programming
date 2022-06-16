426244f2d5fc08c4567af4767dd817417ed8c058
raycast5 <ray.j.colon@gmail.com>
2022-06-16 02:14:15 +0000

    Added file 0 of project 2

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,29 +1,26 @@
 #include "main.h"
 
 /**
- * _strcat - check appends the src string to the dest string,
+ * _strcat - appends the src string to the dest string,
  * overwriting the terminating null byte (\0) at the end of dest,
  * and then adds a terminating null byte
  *
- * Return: Always 0.
+ * @src: the source string
+ * @dest: the destination string
+ * Return: dest string
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int x, index;
-	char temp[107];
-	
-	for (x= 0; src[x] != '\0'; x++)
+	int x, y;
+
+	for (x = 0; dest[x] != '\0'; x++)
 	{
-		temp[index] = src[x];
-		index++;
 	}
-	for (x = 0; dest[x]; x++)
+	for (y = 0; src[y]; y++)
 	{
-		temp[index] = dest[x];
-		index++;
+		dest[x] = src[y];
+		x++;
 	}
-	dest[x] = temp[index];
-	return (src);
 	return (dest);
 }
