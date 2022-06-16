3e769d1e778d663be1c0b9a5a71284bd8b97d18c
raycast5 <ray.j.colon@gmail.com>
2022-06-16 17:57:02 +0000

    Added file 3 of project 2

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,15 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int res;
+	int res = 0;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0' && res == 0; i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		res = s1[i] - s2[i];
+		if (res != 0)
+		{
+			break;
+		}
 	}
 	return (res);
 }
