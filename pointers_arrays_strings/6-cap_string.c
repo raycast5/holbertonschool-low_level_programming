6bc1122123407cdcbc7e8664391a883f7c7d85aa
raycast5 <ray.j.colon@gmail.com>
2022-06-17 00:02:33 +0000

    Added file 6 of project 2

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalize each word on sting
+ * @s: string to be converted
+ * Return: @s
+ */
+
+char *cap_string(char *s)
+{
+	int i, j, class;
+	char sep[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', ' ', '\n', '\t'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] > 96 && s[i] < 123)
+		{
+			class = 0;
+			for (j = 0; class == 0 && j < 13; j++)
+			{
+				if (s[i - 1] == sep[j])
+				{
+					class = 1;
+					s[i] -= 32;
+				}
+				else
+				{
+					class = 0;
+				}
+			}
+		}
+	}
+	return (s);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -32,7 +32,7 @@ void reverse_array(int *a, int n);
 
 char *string_toupper(char *s);
 
-char *cap_string(char *);
+char *cap_string(char *s);
 
 char *leet(char *);
 
