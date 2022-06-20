8d7a5bcb3aea89f75b525857da8782dadf9a4933
raycast5 <ray.j.colon@gmail.com>
2022-06-20 03:52:43 +0000

    Added file 7

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -10,8 +10,9 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j;
-	float s1, s2;
+	int i, j, s1, s2;
+	s1 = 0;
+	s2 = 0;
 
 	for (i =  0; i < size; i++)
 	{
@@ -21,5 +22,5 @@ void print_diagsums(int *a, int size)
 	{
 		s2 += a[(size - 1) * (j + 1)];
 	}
-	printf("%.0f, %.0f\n", s1, s2);
+	printf("%d, %d\n", s1, s2);
 }
