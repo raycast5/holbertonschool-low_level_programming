5d83b8a6bf8572c62ad445a8da787c961b76166b
raycast5 <ray.j.colon@gmail.com>
2022-06-23 23:29:40 +0000

    Added file 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to the program
+ *
+ * @argc: length of @argv
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+		printf("%i\n", argc);
+	return (0);
+}
