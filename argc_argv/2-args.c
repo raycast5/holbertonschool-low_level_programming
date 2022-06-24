76410d6a36453c6ecbe65484182d7dc8b3b572d1
raycast5 <ray.j.colon@gmail.com>
2022-06-24 00:09:51 +0000

    Added file 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ *
+ * @argc: length of @argv
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
