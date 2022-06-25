e95de6e798014aa492db41f45692e3d1c53bcc56
raycast5 <ray.j.colon@gmail.com>
2022-06-25 20:31:40 +0000

    Added file 3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - create array of a certain @width and @height.
+ *
+ * @width: width of array
+ * @height:height of array
+ * Return: NULL if width or height <= 0, arr if success
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		arr = malloc(sizeof(int *) * width);
+		if (arr == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0; i < width; i++)
+		{
+			arr[i] = malloc(sizeof(int *) * height);
+			if (arr[i] == NULL)
+			{
+				return (NULL);
+			}
+		}
+	}
+	return (arr);
+}
