659cba0185ae83823f850acb155535a1f5558d85
raycast5 <ray.j.colon@gmail.com>
2022-06-26 00:35:00 +0000

    Fixed file 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -26,7 +26,7 @@ int **alloc_grid(int width, int height)
 		{
 			return (NULL);
 		}
-		for (i = 0; i < width; i++)
+		for (i = 0; i < height; i++)
 		{
 			arr[i] = malloc(sizeof(int) * width);
 			if (arr[i] == NULL)
@@ -46,7 +46,6 @@ int **alloc_grid(int width, int height)
 				arr[x][i] = 0;
 			}
 		}
-		
 	}
 	return (arr);
 }
