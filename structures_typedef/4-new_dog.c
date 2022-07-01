1b02cb347135ac52517f48ec258c49ebe44d6142
raycast5 <ray.j.colon@gmail.com>
2022-07-01 06:49:40 +0000

    hotfix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,12 +3,45 @@
 #include <stdlib.h>
 
 /**
- * new_dog - creates a new dog
+ * clonestr - clones string
  *
+ * @s1: string to be copied
+ * Return: s2
+ */
+
+char *clonestr(char *s1)
+{
+	int i, len1;
+	char *s2;
+
+	if (s1 != NULL)
+	{
+		for (len1 = 0; s1[len1]; len1++)
+		{
+		}
+		s2 = malloc(sizeof(char) * len1);
+			if (s1 == NULL)
+			{
+				free(s1);
+				return (NULL);
+			}
+		for (i = 0; i <= len1; i++)
+		{
+			s2[i] = s1[i];
+		}
+	return (s2);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
+
+/**
+ * new_dog - creates a new dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
- *
  * Return: Always 0.
  */
 
@@ -17,15 +50,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *nd;
 
 	nd = malloc(sizeof(dog_t));
-		if (nd == NULL)
-		{
-			free(nd->owner);
-			free(nd->name);
-			free(nd);
-			return (NULL);
-		}
-	nd->name = name;
+
+	nd->name = clonestr(name);
+	if (nd->name == NULL)
+	{
+		free(nd->name);
+		return (NULL);
+	}
 	nd->age = age;
-	nd->owner = owner;
+	nd->owner = clonestr(owner);
+	if (nd->owner == NULL)
+	{
+		free(nd->owner);
+		free(nd);
+	}
 	return (nd);
 }
