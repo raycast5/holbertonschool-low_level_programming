058c3876bb7a4e1cdfa7788beb15a1b7c2128aab
raycast5 <ray.j.colon@gmail.com>
2022-07-01 01:18:34 +0000

    Added file 0

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory is for practicing C structures and typedef
---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef WOOF
+#define WOOF
+
+/**
+ * struct dog - struct containing info from dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
