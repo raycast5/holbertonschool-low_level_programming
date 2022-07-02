f3482c8141faa28f1209fbafa660c5087f7d8f06
raycast5 <ray.j.colon@gmail.com>
2022-07-02 21:16:57 +0000

    Added file 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iterates through array
+ *
+ * @array: array to be iterated
+ * @size: size of array
+ * @action: function to execute
+ * Return: Nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long int i;
+
+	if (size != 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
