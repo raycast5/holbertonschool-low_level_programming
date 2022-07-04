64097e766d42fd2e9910ca405c026b8f0d986db0
raycast5 <ray.j.colon@gmail.com>
2022-07-04 22:54:17 +0000

    Fixed for null string

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -21,10 +21,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, char *);
-		printf("%s", str);
-		if (str == NULL)
+		if (str != NULL)
 		{
-			printf("nil");
+			printf("%s", str);
+		}
+		else
+		{
+			printf("(nil)");
 		}
 		if (separator != NULL && i < x)
 		{
