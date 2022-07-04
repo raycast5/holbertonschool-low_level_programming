dc127aefacf9d51d2adabd56f413960f02f38395
raycast5 <ray.j.colon@gmail.com>
2022-07-04 20:55:23 +0000

    Added file 0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sums the given arguments
+ *
+ * @n: number of arguments
+ * Return: sum of all arguments
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum;
+	unsigned int i;
+	va_list args;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		va_start(args, n);
+
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(args, int);
+		}
+		va_end(args);
+	}
+
+return (sum);
+}
---
+++ variadic_functions/README.md
@@ -0,0 +1,1 @@
+This directory is to practice the use of variadic functions
