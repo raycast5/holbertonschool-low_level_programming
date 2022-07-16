24918060cd91fa4cad7a05acef4bcb5c1504105d
raycast5 <ray.j.colon@gmail.com>
2022-07-16 08:37:42 +0000

    Added file 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: head of list
+ * Return: Nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
