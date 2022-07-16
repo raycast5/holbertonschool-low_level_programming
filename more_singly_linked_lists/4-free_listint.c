2ec2b58d5fac1416c3129bf922795c04110e21d7
raycast5 <ray.j.colon@gmail.com>
2022-07-16 21:54:04 +0000

    Added file 4

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees a list
+ * @head: head of list
+ * Return: Nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
