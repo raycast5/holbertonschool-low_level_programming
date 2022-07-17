5bb3e84e204da4fab4997ec18ac867a9b6e4e2e5
raycast5 <ray.j.colon@gmail.com>
2022-07-17 03:08:59 +0000

    Fixed file 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -24,26 +24,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	new_node->n = n;
 	new_node->next = NULL;
-	if (!*head)
+	for (i = 1; i < idx && current; i++)
 	{
-		*head = new_node;
-		return (new_node);
+		current = current->next;
+	}
+	if (!current)
+	{
+		return (NULL);
 	}
 	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
-		return new_node;
+		return (new_node);
 	}
-	for (i = 0; current->next != NULL; i++)
-	{
-		if (i == idx - 1)
-		{
-			new_node->next = current->next;
-			current->next = new_node;
-			return (new_node);
-		}
-		current = current->next;
-	}
+	new_node->next = current->next;
+	current->next = new_node;
+
 return (new_node);
 }
