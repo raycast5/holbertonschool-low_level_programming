b004e96f12e0e42255fc06c09686519f9d225456
raycast5 <ray.j.colon@gmail.com>
2022-07-17 00:15:47 +0000

    Added file 6

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * pop_listint - deletes head
+ * @head: head of list
+ * Return: data of head
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *current;
+	int n;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		current = *head;
+		n = (*head)->n;
+		*head = current->next;
+
+		free(current);
+	}
+return (n);
+}
