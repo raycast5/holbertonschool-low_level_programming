20427cf3c114900b515026a9acfe70bf0009c718
raycast5 <ray.j.colon@gmail.com>
2022-07-17 03:14:55 +0000

    Fixed file 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
-	listint_t *current = *head;
+	listint_t *current;
 	unsigned int i;
 
 	if (!new_node)
@@ -24,6 +24,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	new_node->n = n;
 	new_node->next = NULL;
+	if (!*head)
+	{
+		new_node = *head;
+	}
+	current = *head
 	for (i = 1; i < idx && current; i++)
 	{
 		current = current->next;
