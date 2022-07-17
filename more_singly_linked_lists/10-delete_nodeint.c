8433674cd215b728e03f5db97732e3e7b1a2c2d6
raycast5 <ray.j.colon@gmail.com>
2022-07-17 03:51:55 +0000

    Added file 10

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes node at specefied index
+ *
+ * @head: address of head
+ * @index: index of node
+ * Return: address of the new element, or NULL if it failed
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *current, *temp;
+	unsigned int i;
+
+	if (!*head || !head)
+	{
+		return (-1);
+	}
+	current = *head;
+	if (index == 0)
+	{
+		current = (*head)->next;
+		free(*head);
+		*head = current;
+		return (1);
+	}
+	for (i = 1; i < index && current; i++)
+	{
+		temp = current;
+		current = current->next;
+	}
+	temp->next = current->next;
+	free(current);
+return (1);
+}
