fdfd59836bccefe151e9cdaf8a0b9558cc9df088
raycast5 <ray.j.colon@gmail.com>
2022-07-17 03:23:09 +0000

    Fixed file 9

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->next = NULL;
 	if (!*head)
 	{
-		new_node = *head;
+		*head = new_node;
 		return (new_node);
 	}
 	current = *head;
