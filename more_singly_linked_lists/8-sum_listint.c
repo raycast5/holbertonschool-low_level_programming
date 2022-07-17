52048218d9f455fe12a8304a74e320d037c3f5c6
raycast5 <ray.j.colon@gmail.com>
2022-07-17 01:17:24 +0000

    Added file 8

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_listint - sums the contents of the list
+ * @head: list to be summed
+ * Return: sum or 0 if list is empty
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *current = head;
+	int sum = 0;
+
+	if (!current->next)
+	{
+		return (0);
+	}
+	else
+	{
+		while (current)
+		{
+			sum += current->n;
+			current = current->next;
+		}
+	}
+return (sum);
+}
