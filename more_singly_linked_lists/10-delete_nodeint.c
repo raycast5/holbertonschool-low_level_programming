dcedd4f92368afbc635a4e108b89e23c863409ed
raycast5 <ray.j.colon@gmail.com>
2022-07-17 03:54:36 +0000

    Added file 10

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -33,7 +33,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		temp = current;
 		current = current->next;
 	}
-	temp->next = current->next;
-	free(current);
-return (1);
+	if (current)
+	{
+		temp->next = current->next;
+		free(current);
+		return (1);
+	}
+return (-1);
 }
