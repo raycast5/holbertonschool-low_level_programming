5ef179f28a3533b94b6ca51c62e02aaf0c207d3b
raycast5 <ray.j.colon@gmail.com>
2022-07-20 08:29:30 +0000

    Added file 1

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - prints the binary value of @n
+ * @n: number to convert to binary
+ * Return: Nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	int res, i, flag = 0;
+
+	if (n)
+	{
+		for (i = 31; i >= 0; i--)
+		{
+			res = n >> i;
+
+			if (res & 1)
+			{
+				_putchar('1');
+				flag = 1;
+			}
+			else if (flag == 1)
+				_putchar('0');
+		}
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
