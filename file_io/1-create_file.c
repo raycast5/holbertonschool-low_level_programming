6fd094cca02851ae375f26a00729bc92cb78c3bf
raycast5 <ray.j.colon@gmail.com>
2022-07-22 04:59:12 +0000

    Fixed for NULL text

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,22 +15,25 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	int len = strlen(text_content);
+	int fd, i;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 00600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_content)
 	{
-		write(fd, text_content, len);
+		for (i = 0; text_content[i]; i++)
+		{
+		}
+		write(fd, text_content, i);
 		if (errno)
 			return (-1);
 	}
+
 	close(fd);
 return (1);
 }
