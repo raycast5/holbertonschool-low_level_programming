8e7a911d927627c0258131126fc0da2a8a01cc47
raycast5 <ray.j.colon@gmail.com>
2022-07-22 17:16:03 +0000

    Added file 0, README and header

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - prints contents of list of ints
+ *
+ * @h: list to be printed.
+ * Return: Number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count;
+	dlistint_t *current = (listint_t *)h;
+
+	for (count = 0; current != NULL; count++)
+	{
+		if (current->n)
+		{
+			printf("%d\n", current->n);
+		}
+		current = current->next;
+	}
+return (count);
+}
