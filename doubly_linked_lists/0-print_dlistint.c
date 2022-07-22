c7a69c22b5bf863f73c9fa6c031eb6aa2307193d
raycast5 <ray.j.colon@gmail.com>
2022-07-22 17:17:28 +0000

    Fixed file 0 for compilation

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -13,7 +13,7 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count;
-	dlistint_t *current = (listint_t *)h;
+	dlistint_t *current = (dlistint_t *)h;
 
 	for (count = 0; current != NULL; count++)
 	{
