fef672cd3087afd8e18f7b335af7b77b0d238cc3
raycast5 <ray.j.colon@gmail.com>
2022-07-22 18:21:20 +0000

    Fixed file 3 for betty

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -15,7 +15,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 	dlistint_t *last_node;
-	
+
 	if (!new_node)
 	{
 		return (NULL);
