f80304eebadc61035c6b38a4d16a22a7f30f3982
raycast5 <ray.j.colon@gmail.com>
2022-07-22 05:14:06 +0000

    Added file 2

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,39 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+/**
+ * append_text_to_file - appends text to a file
+ * @filename: name of file
+ * @text_content: content of file
+ * Return: 1 on success or -1 if it fails.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, i;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_APPEND | O_RDWR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (i = 0; text_content[i]; i++)
+		{
+		}
+		write(fd, text_content, i);
+		if (errno)
+			return (-1);
+	}
+
+	close(fd);
+return (1);
+}
