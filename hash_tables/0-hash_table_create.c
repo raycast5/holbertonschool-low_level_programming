e8479baa1d562017390d057520cf480c46ffc8a4
raycast5 <ray.j.colon@gmail.com>
2022-08-22 04:23:19 +0000

    Added dir and file 0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * hash_table_create - function to create a hash table
+ * @size: size of hash table
+ *
+ *Return: pointer to the newely created hash table in success, otherwise NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	if (!size)
+		return (NULL);
+	table = malloc(sizeof(hash_table_t);
+	if (!table)
+		return (NULL);
+	
+	table->array = malloc(sizeof(hash_node_t) * size));
+	if (!table->array)
+	{
+		free(table);
+		return (NULL);
+	}
+	table->size = size;
+return (table);
+}
