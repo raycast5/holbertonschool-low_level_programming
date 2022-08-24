87357d0a2254f18c8eb1be9779da922f563d63d2
raycast5 <ray.j.colon@gmail.com>
2022-08-24 05:50:31 +0000

    Fixed for duplicate key

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,32 +12,39 @@ int hash_table_set(hash_table_t *ht,
 const char *key, const char *value)
 {
 	unsigned long int idx;
-	hash_node_t *new = NULL;
-	char *data = (char *)value;
+	hash_node_t *new = NULL, *current = NULL;
 
-	if (!key)
+	if (!key || !*key)
 		return (0);
 
 	idx = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[idx];
 
+	while (current)
+	{
+		if (strcmp(key, current->key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			return (1);
+		}
+		current = current->next;
+	}
 	new = malloc(sizeof(hash_node_t));
 	if (!new)
 		return (0);
 
 	new->key = (char *)key;
-	new->value = data;
+	new->value = strdup(value);
 	new->next = NULL;
-
-	if (ht->array[idx])
+	if (current)
 	{
 		new->next = ht->array[idx];
 		ht->array[idx] = new;
 	}
 	else
+	{
 		ht->array[idx] = new;
-
+	}
 return (1);
 }
-
-
-
